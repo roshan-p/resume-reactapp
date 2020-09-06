231889b1458b3c36c4113c888e554b1d83b7f893
roshan-p <roshan007191@gmail.com>
2020-09-06 11:31:39 +0700

    update build

---
+++ build/precache-manifest.feb2b834ddc8f8123a4bc508ac004202.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b469e66d97546030bafe7fb3218dc99",
+    "url": "/resume-reactapp/index.html"
+  },
+  {
+    "revision": "2f49e1f2b65d9d2f8163",
+    "url": "/resume-reactapp/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "d65150e0d82d00db96fd",
+    "url": "/resume-reactapp/static/css/main.bd66be21.chunk.css"
+  },
+  {
+    "revision": "2f49e1f2b65d9d2f8163",
+    "url": "/resume-reactapp/static/js/2.39eab256.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/resume-reactapp/static/js/2.39eab256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65150e0d82d00db96fd",
+    "url": "/resume-reactapp/static/js/main.e0d5b585.chunk.js"
+  },
+  {
+    "revision": "bd3351bc9ff9810cb129",
+    "url": "/resume-reactapp/static/js/runtime-main.22b87991.js"
+  }
+]);
